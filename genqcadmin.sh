b389073393a0c6e3af6ab126d789b1bb6cf82ed1
ysicing <i@ysicing.me>
2023-05-16 10:29:08 +0800

    release qcadmin(quickon) 2.0.6 by bot 2023-05-16-10-29

--- genqcadmin.sh
+++ genqcadmin.sh
@@ -4,10 +4,11 @@ set -e
 
 tag=${1}
 [ -d /tmp/quickon ] && rm -rf /tmp/quickon || true
-git clone https://github.com/easysoft/quickon_cli -b ${tag} /tmp/quickon
+git clone https://github.com/easysoft/quickon_cli /tmp/quickon
 cp /tmp/quickon/docs/qcadmin.rb .
 git add .
 version=$(cat qcadmin.rb | grep "version " | awk -F\" '{print $2}')
+echo "tag: ${tag} version: ${version}"
 datetime=$(date +%F-%H-%M)
 git commit -m "release qcadmin(quickon) ${version} by bot ${datetime}"
 git push origin master
