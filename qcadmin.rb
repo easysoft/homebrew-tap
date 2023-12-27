85ea6e59a54fb2dfee3e5bc14e691b2d57460dde
ysicing <i@ysicing.me>
2023-12-27 09:12:21 +0800

    release qcadmin(quickon) 3.0.12 by bot 2023-12-27-09-12

--- qcadmin.rb
+++ qcadmin.rb
@@ -1,12 +1,12 @@
 class Qcadmin < Formula
     desc "qcadmin is an open-source lightweight cli tool for managing quickon."
     homepage "https://github.com/easysoft/quickon_cli"
-    version "3.0.9"
+    version "3.0.12"
 
     on_macos do
       if Hardware::CPU.arm?
         url "https://github.com/easysoft/quickon_cli/releases/download/v#{version}/qcadmin_darwin_arm64"
-        sha256 "a3211d0dd2dded7d9130a6b7f3521984ab8a7d1700acc27587e766e69f264fd8"
+        sha256 "84a7c50ab3589450db5c6fe3b87a6d1159badc87477debd9e621502b597413e1"
 
         def install
             bin.install "qcadmin_darwin_arm64" => "qcadmin"
@@ -15,7 +15,7 @@ class Qcadmin < Formula
 
       if Hardware::CPU.intel?
         url "https://github.com/easysoft/quickon_cli/releases/download/v#{version}/qcadmin_darwin_amd64"
-        sha256 "2cc7aa233d4ae42cde7ebe3f268c2726b166e9be536676ebad44647b712e5fb9"
+        sha256 "2fc1fa43d2a43e7cd1e24062c602481951a2410aeb15d70fe67b479a67914ac8"
 
         def install
             bin.install "qcadmin_darwin_amd64" => "qcadmin"
@@ -26,7 +26,7 @@ class Qcadmin < Formula
     on_linux do
       if Hardware::CPU.intel?
         url "https://github.com/easysoft/quickon_cli/releases/download/v#{version}/qcadmin_linux_amd64"
-        sha256 "a8a84fb9983d205747bba5be29359bcc57389e3deff9a2267f24ffc3595297c0"
+        sha256 "2a017271d153e751b0bb4b5afa8f9414b345715f71c1c8dca4a076d9f215231b"
 
         def install
             bin.install "qcadmin_linux_amd64" => "qcadmin"
@@ -35,7 +35,7 @@ class Qcadmin < Formula
 
       if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
         url "https://github.com/easysoft/quickon_cli/releases/download/v#{version}/qcadmin_linux_arm64"
-        sha256 "f5b3114544c02d3490a8d95297f5221e238265c183e03743508a275e7e049e1a"
+        sha256 "0ab3e76450a1154f5290ab7e221cd47dd5583385c3bb5e7d0b8c70dd0d7ee1ac"
 
         def install
             bin.install "qcadmin_linux_arm64" => "qcadmin"
